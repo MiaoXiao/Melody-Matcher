aa38848ebb6d78737a3ace206c7c16bd5e234a45
Stanley Cohen <scohe001@sierra-8.cs.ucr.edu>
2015-02-16 15:56:05 -0800

    Removed tests from root directory

--- Web2/script.js
+++
@@ -1,7 +0,0 @@
-var pics = ["tiger.png", "penguin.png", "scorpion.png"];
-var current = 0;
-function butClick() {
-    var image = document.getElementById('myImage');
-    current = (current+1) % pics.length;
-    image.src = pics[current];
-}
