6a66bce7ee9f9437866aded134edba5fcdebf8bb
MiaoXiao <topgb@msn.com>
2015-02-16 20:55:30 -0800

    Choose between violin or piano sounds, mps and ogg for each instrument

--- src/soundManager.js
+++ src/soundManager.js
@@ -1,37 +1,49 @@
 //choose a specific scale
-function chooseScale(){
+function chooseScale() {
+
+}
+
+//creates a random melody
+function generateMelody(numNotes, range) {
+
+}
+
+//check to see if melody is correct so far
+function checkMelody(note) {
 
 }
 
 //load all sounds that will be used
-function loadSounds() {
+//pass in what instrument to use, violin or piano
+function loadSounds(instrument) {
 
 	//register plugins
 	//createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.FlashAudioPlugin]);
 	
 	//audio path
-	var audioPath = "./sounds/piano/";
+	var audioPath = "./sounds/" + instrument + "/";
 	
 	//library of sounds
 	var sounds = [
-		{src: "0_C4.mp3", id: "C4"},
-		{src: "1_Db4.mp3", id: "Db4"},
-		{src: "2_D4.mp3", id: "D4"},
-		{src: "3_Eb4.mp3", id: "Eb4"},
-		{src: "4_E4.mp3", id: "E4"},
-		{src: "5_F4.mp3", id: "F4"},
-		{src: "6_Gb4.mp3", id: "Gb4"},
-		{src: "7_G4.mp3", id: "G4"},
-		{src: "8_Ab4.mp3", id: "Ab4"},
-		{src: "9_A4.mp3", id: "A4"},
-		{src: "10_Bb4.mp3", id: "Bb4"},
-		{src: "11_B4.mp3", id: "B4"},
-		{src: "12_C5.mp3", id: "C5"}
+		{src: "0_C4.ogg", id: "C4"},
+		{src: "1_Db4.ogg", id: "Db4"},
+		{src: "2_D4.ogg", id: "D4"},
+		{src: "3_Eb4.ogg", id: "Eb4"},
+		{src: "4_E4.ogg", id: "E4"},
+		{src: "5_F4.ogg", id: "F4"},
+		{src: "6_Gb4.ogg", id: "Gb4"},
+		{src: "7_G4.ogg", id: "G4"},
+		{src: "8_Ab4.ogg", id: "Ab4"},
+		{src: "9_A4.ogg", id: "A4"},
+		{src: "10_Bb4.ogg", id: "Bb4"},
+		{src: "11_B4.ogg", id: "B4"},
+		{src: "12_C5.ogg", id: "C5"}
 	];
+	createjs.Sound.alternateExtensions = ["mp3"];
 	
 	//loop through sounds array
 	for (var i = 0; i < sounds.length; i++) {
-		createjs.Sound.registerSound(audioPath + sounds[i].src, sounds[i].id, 50);
+		createjs.Sound.registerSound(audioPath + sounds[i].src, sounds[i].id, 25);
 	}
 }
 
