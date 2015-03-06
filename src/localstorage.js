99f3529ac757c8251c3774e62164a2731768001a
Sherina Bala <sbala003@ucr.edu>
2015-03-06 00:28:10 -0800

    local storage
    started but not yet working

---
+++ src/localstorage.js
@@ -0,0 +1,76 @@
+/*
+//check browser for localStorage and sessionStorage support
+if(typeof(Storage) !== "undefined") {
+    document.getElementById("test").innerHTML = "localStorage and sessionStorage support available";
+} else {
+    document.getElementById("test").innerHTML = "no web storage support";
+}
+*/
+//calls all function to store in local storage; possibly go in the if function
+
+
+var finalGameInfo = JSON.parse(localStorage.getItem("stats"));
+successfulMelodies();
+//document.getElementById("test1").innerHTML = "successful melodies: " + x;//SESSION.successfulmelodies;
+//var x = successfulMelodies();
+//OVERALL.updateSessions();
+//localStorage.setItem("storedstats", JSON.stringify(OVERALL));
+
+
+//-------------------------- TO DO LATER--------------------------------------------------
+//figure out a faster way of testing everything
+//-----------------------------------------------------------------------------------------
+
+
+var SESSION = {
+	//date: Date(), 
+	successfulmelodies: 0
+	
+};
+/*
+var OVERALL = {
+	//best: 0,
+	allSessions: [],
+	updateSessions: function() {
+		//push completed session information to array
+		this.allSessions.push(SESSION);
+	}
+	
+};
+*/
+//returns the number of successful melodies for one session
+function successfulMelodies() {
+	//document.getElementById("test1").innerHTML = "successful melodies: ";
+	SESSION.successfulmelodies = finalGameInfo.allMelodies.length;
+	document.getElementById("test1").innerHTML = "successful melodies: ";
+	
+}
+
+/*
+//returns the number of successful melodies total
+
+
+//returns longest streak of days played
+
+//returns longest streak of melodies with no errors
+
+//returns longest streak of melodies with melody played once
+
+//returns longest streak of melodies solved under 10 seconds
+
+//returns longest streak of melodies solved under 5 seconds
+
+//returns the longest session played (minutes and date)
+
+//return best score ever
+
+//returns average score overall
+
+//returns accuracy in one session
+
+//average number of times a melody is played per melody
+
+//the most times a melody was played
+
+*/
+
