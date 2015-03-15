86d9dcd826465039d4df0896f1769d90252a93c0
Ace Rhino <a12n@mac.com>
2015-03-15 11:55:36 -0700

    Updated to match master branch

--- interface.js
+++ interface.js
@@ -1,13 +1,7 @@
 //select difficulty based on radial button
-function dif_selected() {
-    //Enable the start button if it wasn't already
-    document.getElementById("btn01").disabled = false;
+function dif_selected(difficulty) {
     //Grab the radio form and loop through it looking for the selected btn
-    var rdio = document.getElementById("dif_radio");
-    for (var i = 0; i < rdio.length ;i++) {
-        //Set difficulty accordingly
-        if(rdio.elements[i].checked) sessionStorage.setItem("difficulty", i*10);
-    }
+    sessionStorage.setItem("difficulty", parseInt(difficulty));
 }
 
 //start the game by switching html pages (should be removed)
@@ -16,6 +10,14 @@ function letsgo() {
     document.getElementById("display").classList.add('in');
     document.getElementById("start_screen").classList.add('out');
     document.getElementById("main").classList.add('start');
+    
+    var key = document.getElementById("key_type");
+    var key_val = key.options[key.selectedIndex].value;
+    
+    var scale = document.getElementById("scale_type");
+    var scale_val = scale.options[scale.selectedIndex].value;
+    
+    chooseScale(key_val+scale_val);
 }
 
 //change the volume
